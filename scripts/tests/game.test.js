324811d252b662b719a331a1711eda0160f8a5c3
Jessica Fransson <jessicafransson@outlook.com>
2022-09-30 14:18:57 +0000

    added showscore

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -1,11 +1,8 @@
-
-
 /**
  * @jest-environment jsdom
  */
 
- const { game } = require("../game");
-
+ const { game, newGame, showScore, addTurn, lightsOn } = require("../game");
 
  beforeAll(() => {
      let fs = require("fs");
@@ -30,5 +27,50 @@
      });
      test("choices contain correct ids", () => {
          expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+     });
+ });
+ 
+ describe("newGame works correctly", () => {
+     beforeAll(() => {
+         game.score = 42;
+         game.playerMoves = ["button1", "button2"];
+         game.currentGame = ["button1", "button2"];
+         document.getElementById("score").innerText = "42";
+         newGame();
+     });
+     test("should set game score to zero", () => {
+         expect(game.score).toEqual(0);
+     });
+     test("should display 0 for the element with id of score", () => {
+         expect(document.getElementById("score").innerText).toEqual(0);
+     });
+     test("should clear the player moves array", () => {
+         expect(game.playerMoves.length).toBe(0);
+     });
+     test("should add one move to the computer's game array", () => {
+         expect(game.currentGame.length).toBe(1);
+     });
+ });
+ 
+ describe("gameplay works correctly", () => {
+     beforeEach(() => {
+         game.score = 0;
+         game.currentGame = [];
+         game.playerMoves = [];
+         addTurn();
+     });
+     afterEach(() => {
+         game.score = 0;
+         game.currentGame = [];
+         game.playerMoves = [];
+     });
+     test("addTurn adds a new turn to the game", () => {
+         addTurn();
+         expect(game.currentGame.length).toBe(2);
+     });
+     test("should add correct class to light up the buttons", () => {
+         let button = document.getElementById(game.currentGame[0]);
+         lightsOn(game.currentGame[0]);
+         expect(button.classList).toContain(game.currentGame[0] + "light");
      });
  });
